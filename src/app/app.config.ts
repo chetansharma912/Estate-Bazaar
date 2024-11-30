a9b81d596819a7bb4d8a8bcde56a8c9e864bea7a
Chetan Sharma <chetansharma911912@gmail.com>
2024-11-30 11:31:00 +0530

    fire base deployed files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBOP5oRSiEdpHdMtgs7agXCKtTqxTMWuxM",
+  authDomain: "estate-bazaar-ee44e.firebaseapp.com",
+  projectId: "estate-bazaar-ee44e",
+  storageBucket: "estate-bazaar-ee44e.firebasestorage.app",
+  messagingSenderId: "62197188040",
+  appId: "1:62197188040:web:ebfada8c91f46e960cacfd",
+  measurementId: "G-W0YW043DHW"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes)
+  ]
 };
